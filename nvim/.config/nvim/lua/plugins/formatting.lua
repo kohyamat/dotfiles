68cd3530b44f0e4235ab61462ab8625af99fcf21
Tetsuo Kohyama <tetsuo.kohyama@gmail.com>
2026-04-01 16:49:46 +0900

    fix: include R formatting configuration in plugins/formatting.lua

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -16,12 +16,13 @@ return {
     formatters_by_ft = {
       sh = { "shfmt" },
       lua = { "stylua" },
-      -- Ruff (extremely fast and combines isort + black)
       python = { "ruff_organize_imports", "ruff_format" },
       rust = { "rustfmt", lsp_format = "fallback" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
       typescript = { "prettierd", "prettier", stop_after_first = true },
       vue = { "prettierd", "prettier", stop_after_first = true },
+      -- R: styler を使用
+      r = { "styler" },
       ["_"] = { "trim_whitespace" },
     },
     default_format_opts = {
@@ -31,6 +32,7 @@ return {
       shfmt = { prepend_args = { "-i", "2" } },
       stylua = { prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" } },
       styler = {
+        -- R環境の styler パッケージを使用
         args = { "-s", "-e", "styler::style_file(commandArgs(TRUE))", "--args", "$FILENAME" },
         stdin = false,
       },
