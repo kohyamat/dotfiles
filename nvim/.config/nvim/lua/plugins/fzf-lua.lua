81b46e317beba948c4e16c2afed7c1c4b148047f
Tetsuo Kohyama <tetsuo.kohyama@gmail.com>
2026-04-02 13:45:37 +0900

    refactor: revert fzf-lua to stable on_create keymaps

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -4,52 +4,31 @@ return {
   opts = {},
   config = function()
     local fzf = require("fzf-lua")
-    local config = require("fzf-lua.config")
-    local actions = require("trouble.sources.fzf").actions
-
-    -- 検索結果のデフォルトアクションに Trouble を追加
-    config.defaults.actions.files["ctrl-t"] = actions.open
 
     fzf.setup({
-      -- 基本的な検索動作の設定
+      -- エラー回避のため fzf 側の高度なキー生成を無効化
       keymap = {
-        builtin = {
-          ["<C-g>"] = "toggle-preview",
-          ["<F1>"]  = "help",
-        },
-        fzf = {
-          -- 移動
-          ["ctrl-n"] = "down",
-          ["ctrl-p"] = "up",
-          ["ctrl-j"] = "down",
-          ["ctrl-k"] = "up",
-          -- プレビュー
-          ["ctrl-u"] = "preview-page-up",
-          ["ctrl-d"] = "preview-page-down",
-          -- 選択と決定 (明示的に設定)
-          ["enter"]  = "accept",
-          ["tab"]    = "toggle-dt-sign", -- 複数選択の切り替え
-          ["ctrl-a"] = "select-all",
-        },
+        fzf = false,
       },
       winopts = {
-        height = 0.85,
-        width = 0.80,
-        preview = {
-          layout = "vertical",
-          vertical = "up:45%",
-        },
+        -- Neovim側で確実にキー入力を矢印キーに変換する
+        on_create = function()
+          vim.keymap.set("t", "<C-n>", "<Down>", { silent = true, buffer = true })
+          vim.keymap.set("t", "<C-p>", "<Up>", { silent = true, buffer = true })
+          vim.keymap.set("t", "<C-j>", "<Down>", { silent = true, buffer = true })
+          vim.keymap.set("t", "<C-k>", "<Up>", { silent = true, buffer = true })
+        end,
       },
     })
 
-    -- キーマップ
+    -- ノーマルモードの起動用キーマップ
     vim.keymap.set("n", "<leader>fa", fzf.builtin, { desc = "fzf-lua builtin" })
     vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "find files" })
     vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "live grep" })
     vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "buffers" })
     vim.keymap.set("n", "<leader>fr", fzf.resume, { desc = "resume last search" })
 
-    -- LSP
+    -- LSP / 診断関連
     vim.keymap.set("n", "<leader>fd", fzf.lsp_document_diagnostics, { desc = "diagnostics (current file)" })
     vim.keymap.set("n", "<leader>fD", fzf.lsp_workspace_diagnostics, { desc = "diagnostics (workspace)" })
     vim.keymap.set("n", "<leader>fs", fzf.lsp_document_symbols, { desc = "document symbols" })
