973da09999f0c13b14ee8f077c793d83c2af702f
Tetsuo Kohyama <tetsuo.kohyama@gmail.com>
2026-04-02 12:00:17 +0900

    feat: apply recommended keymaps and layout for fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,67 +1,54 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "folke/trouble.nvim" },
-  opts = {
-    -- UI を rounded に統一
-    winopts = {
-      height = 0.85,
-      width = 0.80,
-      row = 0.35,
-      col = 0.50,
-      border = "rounded",
-      preview = {
-        layout = "vertical",
-        vertical = "down:45%",
-      },
-    },
-    -- 標準的な fzf の動作 (Ctrl-j/k での移動、Ctrl-q で Quickfix 等)
-    keymap = {
-      builtin = {
-        ["<C-d>"] = "preview-page-down",
-        ["<C-u>"] = "preview-page-up",
-      },
-      fzf = {
-        ["CTRL-Q"] = "select-all+accept", -- すべて選択して Quickfix 等へ
-      },
-    },
-    fzf_opts = {
-      ["--ansi"] = "",
-      ["--info"] = "inline",
-      ["--height"] = "100%",
-      ["--layout"] = "reverse",
-      ["--border"] = "none",
-    },
-  },
-  config = function(_, opts)
+  opts = {},
+  config = function()
     local fzf = require("fzf-lua")
     local actions = require("trouble.sources.fzf").actions
-    
-    -- Trouble との統合キーマップ
-    opts.keymap.builtin["<C-t>"] = actions.open
-    fzf.setup(opts)
 
-    -- 1. ファイル検索 (標準的)
-    vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find Files" })
-    vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Find Buffers" })
-    vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { desc = "Find Old Files" })
-    vim.keymap.set("n", "<C-p>", fzf.files, { desc = "Find Files (Shortcut)" })
+    fzf.setup({
+      -- 推奨キーマップの設定
+      keymap = {
+        builtin = {
+          -- fzf-lua本体の操作
+          ["<C-t>"] = actions.open,      -- 検索結果を Trouble に送る
+          ["<C-g>"] = "toggle-preview",  -- プレビューの表示/非表示
+          ["<F1>"]  = "help",            -- ヘルプの表示
+        },
+        fzf = {
+          -- fzfプロセス側の操作 (入力中)
+          ["ctrl-n"] = "down",           -- 次の候補
+          ["ctrl-p"] = "up",             -- 前の候補
+          ["ctrl-j"] = "down",           -- 次の候補 (Vimスタイル)
+          ["ctrl-k"] = "up",             -- 前の候補 (Vimスタイル)
+          ["ctrl-u"] = "preview-page-up",   -- プレビューを上にスクロール
+          ["ctrl-d"] = "preview-page-down", -- プレビューを下にスクロール
+          ["ctrl-a"] = "select-all",     -- 全選択
+        },
+      },
+      -- 見た目とレイアウトの最適化
+      winopts = {
+        height = 0.85,
+        width = 0.80,
+        preview = {
+          layout = "vertical", -- 画面に応じて縦横を自動切替
+          vertical = "up:45%", -- 縦表示の時は上にプレビュー
+        },
+      },
+    })
 
-    -- 2. 文字列検索 (標準的)
-    vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "Search Live Grep" })
-    vim.keymap.set("n", "<leader>sw", fzf.grep_cword, { desc = "Search Word under cursor" })
-    vim.keymap.set("n", "<leader>s/", fzf.blines, { desc = "Search in Current Buffer" })
-    vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "Search Help" })
-    vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "Search Resume" })
+    -- 基本操作のキーマップ
+    vim.keymap.set("n", "<leader>fa", fzf.builtin, { desc = "fzf-lua builtin" })
+    vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "find files" })
+    vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "live grep" })
+    vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "buffers" })
+    vim.keymap.set("n", "<leader>fr", fzf.resume, { desc = "resume last search" })
 
-    -- 3. LSP 統合 (標準の gd, gr を強化)
-    vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "LSP Definitions (Fzf)" })
-    vim.keymap.set("n", "gr", fzf.lsp_references, { desc = "LSP References (Fzf)" })
-    vim.keymap.set("n", "gi", fzf.lsp_implementations, { desc = "LSP Implementations (Fzf)" })
-    vim.keymap.set("n", "<leader>sd", fzf.lsp_document_symbols, { desc = "LSP Document Symbols" })
-    vim.keymap.set("n", "<leader>sD", fzf.lsp_workspace_symbols, { desc = "LSP Workspace Symbols" })
-    vim.keymap.set("n", "<leader>xw", fzf.lsp_workspace_diagnostics, { desc = "LSP Workspace Diagnostics" })
-
-    -- 4. スペルチェック (標準の z= を強化)
-    vim.keymap.set("n", "z=", fzf.spell_suggest, { desc = "Spell Suggestions (Fzf)" })
+    -- LSP / 診断関連
+    vim.keymap.set("n", "<leader>fd", fzf.lsp_document_diagnostics, { desc = "diagnostics (current file)" })
+    vim.keymap.set("n", "<leader>fD", fzf.lsp_workspace_diagnostics, { desc = "diagnostics (workspace)" })
+    vim.keymap.set("n", "<leader>fs", fzf.lsp_document_symbols, { desc = "document symbols" })
+    vim.keymap.set("n", "<leader>gr", fzf.lsp_references, { desc = "find references" })
+    vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "go to definition" })
   end,
 }
